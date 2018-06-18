0ea2ab8229c754c3075f675be98a34c93a4346f5
jisung <jisung.smart@gmail.com>
2018-06-19 06:19:42 +0900

    Add dataController.swift and datamodel.
     To persist data there were need more setting and codes. The first step was to know about DataModel. It's a little bit different from code that I've studied but very useful and easy to understand. In the Data Model set two instances which were pin, photo. Both of them has own entity and relationship.

---
+++ VirtualTourist/Model/DataController.swift
@@ -0,0 +1,12 @@
+//
+//  DataController.swift
+//  VirtualTourist
+//
+//  Created by The book on 2018. 6. 19..
+//  Copyright © 2018년 The book. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
